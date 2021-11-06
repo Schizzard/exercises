2c37a265dc8e28362606306541a8cac6b65a1cfb
ekorzunov <korzunov.e.v@gmail.com>
2021-11-06 17:05:16 +0500

    add comments

--- Python/01. First exercises/ex0006_strings.py
+++ Python/01. First exercises/ex0006_strings.py
@@ -1,8 +1,10 @@
 # Example for using string methods
 
-my_string = "0123456789abcdefghijklmn"
-print(my_string)
-print(my_string.capitalize())
-print(my_string.upper())
-print(my_string.lower())
-print(my_string[1:12:4])
+my_string = "a0B1c2D3e4F5g6H7i8J9k"
+print(my_string)                # a0B1c2D3e4F5g6H7i8J9k
+print(my_string.capitalize())   # A0b1c2d3e4f5g6h7i8j9k
+print(my_string.upper())        # A0B1C2D3E4F5G6H7I8J9K
+print(my_string.lower())        # a0b1c2d3e4f5g6h7i8j9k
+print(my_string[1:12:4])        # 024
+print(my_string[5])             # 2
+
