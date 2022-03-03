93962a7844c821503f86878c7791ab87a2caaaf5
ekorzunov <korzunov.e.v@gmail.com>
2022-03-04 01:59:10 +0500

    new stuff

---
+++ Python/04 Stepik/01 Программирование на Python/Lesson 1.7.py
@@ -0,0 +1,4 @@
+# Steps 2 - 4
+print(int(2.99))
+print(int(-1.6))
+print(9**19 - int(float(9**19)))
---
+++ Python/04 Stepik/01 Программирование на Python/Lesson 1.9.py
@@ -0,0 +1,11 @@
+# Steps 6-8
+'((a and b) or ((not a) and (not b)))'
+
+x = 5
+y = 10
+y > x * x or y >= 2 * x and x < y   # 'True'
+
+
+a = True
+b = False
+a and b or not a and not b
