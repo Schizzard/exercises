63ce348014bf1cefc75628dac6fa134fd5c7239f
ekorzunov <korzunov.e.v@gmail.com>
2022-03-21 16:07:05 +0500

    new stuff

---
+++ Python/02_checkio/03_Electronic_Station/021_unix_match_part_1.py
@@ -0,0 +1,62 @@
+# Unix Match. Part 1
+
+# Sometimes you find yourself in a situation when, among a huge number of files 
+# on your computer or in a separate folder, you need to find files of a certain 
+# type. For example, images with the extension '.jpg' or documents with the 
+# extension '.txt', or even files that have the word 'butterfly' in their name. 
+# Doing this manually can be time-consuming. 'Matching' or patterns for 
+# searching files by a specific mask are just what you need for these sort of 
+# challenges.
+# This mission will help you understand how this works.
+
+# You need to find out if the given unix pattern matches the given filename.
+
+# Let me show you what I mean by matching the filenames in the unix-shell. For 
+# example, * matches everything and *.txt matches all of the files that have 
+# txt extension. Here is a small table that shows symbols that can be used in 
+# patterns.
+
+# *	matches everything
+# ?	matches any single character
+
+# Input: Two arguments. Both are strings.
+# Output: Bool.
+
+# Example:
+# unix_match('somefile.txt', '*') == True
+# unix_match('other.exe', '*') == True
+# unix_match('my.exe', '*.txt') == False
+# unix_match('log1.txt', 'log?.txt') == True
+# unix_match('log12.txt', 'log?.txt') == False
+# unix_match('log12.txt', 'log??.txt') == True
+
+# How it is used: in the unix-shell
+# Precondition: 0 < len(strings) < 100
+
+import fnmatch
+import re
+
+# my solve (first) is not accepted
+def unix_match(filename: str, pattern: str) -> bool:
+    return fnmatch.fnmatch(filename, pattern)
+
+
+# my solve (second)
+def unix_match(filename: str, pattern: str) -> bool:
+    pattern_1 = pattern.replace('.','\\.').replace('*','.*').replace('?','.')
+    match1 = re.match(pattern_1, filename)
+    return True if match1 is not None else False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(unix_match('somefile.txt', '*'))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert unix_match('somefile.txt', '*') == True
+    assert unix_match('other.exe', '*') == True
+    assert unix_match('my.exe', '*.txt') == False
+    assert unix_match('log1.txt', 'log?.txt') == True
+    assert unix_match('log12.txt', 'log?.txt') == False
+    assert unix_match('log12.txt', 'log??.txt') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
