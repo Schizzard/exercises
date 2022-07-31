3fd3d4a6363b08c799a68b5c93d13eed59b01603
ekorzunov <korzunov.e.v@gmail.com>
2022-07-31 17:54:10 +0500

    move

---
+++ Python/06 LeetCode/876. Middle of the Linked List.py
@@ -0,0 +1,54 @@
+# Given the head of a singly linked list, return the middle node of the linked
+# list.
+# If there are two middle nodes, return the second middle node.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+# Example 2:
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+
+# Constraints:
+# The number of nodes in the list is in the range [1, 100].
+# 1 <= Node.val <= 100
+
+# Definition for singly-linked list.
+from tkinter.messagebox import NO
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        a = head
+        b = []
+        while a != None:
+            b.append(a)
+            a = a.next
+        return b[len(b)//2]
+        
+
+
+n1 = ListNode(1)
+n2 = ListNode(2)
+n3 = ListNode(3)
+n4 = ListNode(4)
+n5 = ListNode(5)
+
+n1.next = n2
+n2.next = n3
+n3.next = n4
+n4.next = n5
+n5.next = None
+
+a = Solution
+assert a.middleNode(a, head=n1) == [3,4,5]
